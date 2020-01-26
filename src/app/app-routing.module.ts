242743c3a9c25a004adfb3d4c0ca369ba61971b4
Sega Diop <sega.diop@madi-corp.net>
2020-01-26 23:54:15 +0100

    Technical skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Quizz',
+    },
+    children: [{
+      path: '',
+      loadChildren: './quizz/quizz.module#QuizzModule',
+    }],
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {FooterComponent, HeaderComponent, MainComponent} from './layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
