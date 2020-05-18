001313fb61bb7f2b2186572165e716c0bedd7513
Schalcke <34965313+Schalcke@users.noreply.github.com>
2020-05-18 18:08:05 +0000

    List Quizz

---
+++ src/app/services/quizz/quizz.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const baseUrl = environment.apiUrl + '/quizz';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizzService {
+
+  constructor(private http: HttpClient) { }
+
+  getAllQuizz():Observable<any[]> {
+    return this.http.get(baseUrl + '/all').pipe(
+      map((response:any) => response.data) 
+      );
+  }
+
+}
